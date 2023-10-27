bc0765a5e302e72982a6337dad71af0694395802
tre3 <goddywin@gmail.com>
2023-10-27 11:17:30 +0800

    doing calender display

--- src/components/Calender/Calender.jsx
+++ src/components/Calender/Calender.jsx
@@ -1,16 +1,31 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
-import { Badge } from '@mui/material';
-import { PickersDay } from '@mui/x-date-pickers';
+import { PickersDay } from '@mui/x-date-pickers/PickersDay';
+import CheckIcon from '@mui/icons-material/Check';
+import Badge from '@mui/material/Badge';
+import TextField from '@mui/material/TextField';
 
-
+// reference: https://www.youtube.com/watch?v=BN_wfeG47oQ&t=748s
 
 export default function Calender() {
+  const [date, setDate] = useState(new Date());
+  const [highlightedDays, setHighlightedDays] = useState([1, 2, 13]);
+
+  const handleDateChange = (date) => {
+    console.log('Selected Date:', date.toISOString());
+  };
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <StaticDatePicker orientation="landscape" />
+      <StaticDatePicker
+        orientation="portrait"
+        onChange={handleDateChange}
+        disableFuture
+        
+ 
+      />
     </LocalizationProvider>
   );
 }
