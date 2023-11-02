18d6d36392259850d9de68d10e003af170e36489
tre3 <goddywin@gmail.com>
2023-11-02 16:33:07 +0800

    styled nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,82 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { logOut } from "../../utilities/users-service";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 export default function NavBar({ user, setUser }) {
-	const handleLogout = () => {
-		logOut();
-		setUser(null);
-	};
-	return (
-		<>
-			<nav>
-				<Link to="/calender">Calender</Link>
-				&nbsp; | &nbsp;
-				<Link to="/user/analysis">Analysis</Link>
-				&nbsp; | &nbsp;
-				<Link to="/learn">Learn</Link>
-				&nbsp; | &nbsp;
-				<span>Welcome, {user.name}</span>
-				&nbsp; | &nbsp;
-				<Link to="" onClick={handleLogout}>
-					Log Out
-				</Link>
-			</nav>
-		</>
-	);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    logOut();
+    setUser(null);
+  };
+
+  return (
+    <AppBar position="static" style={{ background: "white" }}>
+      <Toolbar>
+        <img
+          src="https://i.imgur.com/6Rgthpn.png"
+          alt="Logo"
+          width="120"
+          height="auto"
+          style={{ marginRight: "620px" }}
+        />
+        <Typography variant="h6" style={{ color: "black", width: "90%" }}>
+          <span>Welcome, {user.name}</span>
+        </Typography>
+        <Button
+          aria-controls="menu" 
+          aria-haspopup="true"
+          onClick={handleMenuOpen}
+          style={{ color: "black" }}
+        >
+          Menu
+        </Button>
+        <Menu
+          id="menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+        >
+          <MenuItem onClick={handleMenuClose}>
+            <Link to="/calender" style={{ color: "inherit" }}>
+              Calender
+            </Link>
+          </MenuItem>
+          <MenuItem onClick={handleMenuClose}>
+            <Link to="/user/analysis" style={{ color: "inherit" }}>
+              Analysis
+            </Link>
+          </MenuItem>
+          <MenuItem onClick={handleMenuClose}>
+            <Link to="/learn" style={{ color: "inherit" }}>
+              Learn
+            </Link>
+          </MenuItem>
+			<MenuItem>
+			<Button 
+			color="error" 
+			variant="contained"
+			onClick={handleLogout}
+			>
+				Log Out
+			</Button>
+			</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
 }
