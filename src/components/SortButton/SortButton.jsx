06942266435aaf51b1e4007c8edd7e1b990d5024
tre3 <goddywin@gmail.com>
2023-11-02 10:26:55 +0800

    feat: added sort earliest to lastest

---
+++ src/components/SortButton/SortButton.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+export default function SortButton({ sortAscending, setSortAscending, allRecords, setAllRecords }) {
+  const handleSortToggle = () => {
+    handleSortByDate();
+  }
+
+  const handleSortByDate = () => {
+    const sortedRecords = [...allRecords];
+
+    if (sortAscending) {
+      sortedRecords.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+    } else {
+      sortedRecords.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+    }
+
+    setAllRecords(sortedRecords);
+    setSortAscending(!sortAscending);
+  };
+
+  return (
+    <Button onClick={handleSortToggle}>
+      {sortAscending ? 'Earliest Date First' : 'Latest Date First'}
+    </Button>
+  );
+}
